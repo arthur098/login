e5740e78be6fa77a07ed68bfed0a0dea7d5a2196
Arthur Oliveira Rodrigues <arthur-or@detran.go.gov.br>
2019-12-19 17:19:21 -0300

    Implementação header.

--- log/src/main/webapp/src/app/app.module.ts
+++ log/src/main/webapp/src/app/app.module.ts
@@ -8,11 +8,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { RollTopButtonComponent } from './@core/roll-top-button/roll-top-button.component';
 import { MaterialDesignModule } from './@core/material-design/material-design.module';
+import { HeaderComponent } from './pages/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RollTopButtonComponent
+    RollTopButtonComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
--- log/src/main/webapp/src/app/pages/pages-routing.module.ts
+++ log/src/main/webapp/src/app/pages/pages-routing.module.ts
@@ -4,9 +4,8 @@ import { AboutComponent } from './about/about.component';
 import { AuthGuard } from '../guards/auth.guard';
 
 const routes: Routes = [
-    { path: '', redirectTo: 'teste', pathMatch: 'full' },
-    { path: 'teste', loadChildren: () => import('./teste/teste.module').then(m => m.TesteModule)},
-    { path: 'about', component: AboutComponent, canActivate: [ AuthGuard ]},
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'about', component: AboutComponent},
     { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule)},
     { path: 'registrar', loadChildren: () => import('./registrar/registrar.module').then(m => m.RegistrarModule)}
 ];
